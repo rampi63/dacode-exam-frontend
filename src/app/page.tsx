dd384c44be0c70f5cee2a7e6479128c4a4ccd5ba
alberto-ifd <alberto.jolyt@ifd.com.mx>
2025-05-22 19:02:14 -0600

    Add Apollo Client setup and integrate with application
    - Updated package.json and package-lock.json to include Apollo Client and related dependencies.
    - Created ApolloProvider component to wrap application with ApolloProvider.
    - Implemented GraphQL query in the Home component to fetch data.
    - Added .gitignore and vcs.xml for IDE configuration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
+'use client';
+
 import Image from "next/image";
+import { gql, useQuery } from '@apollo/client';
+
+const PING = gql`
+  query {
+    pingAuth,
+    pingUsers,
+  }
+`;
+
 
 export default function Home() {
+
+
+  const { data, loading, error } = useQuery(PING);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error 😢</p>;
+
+  return <p className="mt-10">Respuesta del servidor: {data.pingAuth} {data.pingUsers}</p>;
+
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
