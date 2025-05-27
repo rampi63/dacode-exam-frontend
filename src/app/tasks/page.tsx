1e168a78360d3ab222a4f30a68b6cd3cbb8e623d
alberto-ifd <alberto.jolyt@ifd.com.mx>
2025-05-27 04:25:05 -0600

    fix: remove unnecessary task prop from CreateTaskModal in TasksPage

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -126,7 +126,6 @@ export default function TasksPage() {
 
                 {creatingTask && (
                     <CreateTaskModal
-                        task={creatingTask}
                         onClose={() => setCreatingTask(false)}
                         refetch={refetch}
                     />
