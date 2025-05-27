0ad4933152b1bd9add246bb9ce9af829c17776c0
alberto-ifd <alberto.jolyt@ifd.com.mx>
2025-05-27 04:41:32 -0600

    fix: update redirect from tasks to login on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 export default function Home() {
 
   useEffect(() => {
-    window.location.href = "/tasks";
+    window.location.href = "/login";
   }, []);
 
 }
